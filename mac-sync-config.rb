16ad237e4a2cc59ec831475a064b09f21fc27965
Gyubong <jopemachine@naver.com>
2022-12-04 15:52:11 +0900

    Add formula

---
+++ mac-sync-config.rb
@@ -0,0 +1,21 @@
+class MacSyncConfig < Formula
+  desc "Sync your config files between macs through your Github repository."
+  homepage "https://github.com/jopemachine/mac-sync-config"
+  url "https://github.com/jopemachine/mac-sync-config.git", :tag => "v0.1.0"
+  license "MIT"
+
+  depends_on "go" => :build
+
+	head do
+    url "https://github.com/jopemachine/mac-sync-config.git", :branch => "master"
+  end
+
+  def install
+    system "go", "build", "."
+    bin.install "mac-sync-config"
+  end
+
+  test do
+    system "#{HOMEBREW_PREFIX}/bin/mac-sync-config", "--version"
+  end
+end
